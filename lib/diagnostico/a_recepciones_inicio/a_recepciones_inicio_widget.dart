0481ec32dccb2ad5a01939e83a21dfb91306d8c5
unknown <andres@zerocode.la>
2026-07-25 18:28:43 -0500

    Fix: add missing FlutterFlowIconButton import

--- lib/diagnostico/a_recepciones_inicio/a_recepciones_inicio_widget.dart
+++ lib/diagnostico/a_recepciones_inicio/a_recepciones_inicio_widget.dart
@@ -3,6 +3,7 @@ import '/backend/backend.dart';
 import '/components/tipo_recepcion_widget.dart';
 import '/components/vacio_widget.dart';
 import '/flutter_flow/flutter_flow_drop_down.dart';
+import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/form_field_controller.dart';
