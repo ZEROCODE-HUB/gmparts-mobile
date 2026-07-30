ff5ca1e9599e421d8d7e513e351286af86a2c1c5
unknown <andres@zerocode.la>
2026-07-29 19:48:43 -0500

    fix: agregar parentesis faltante en Padding (crear_cotizacion_funcionando_widget)

--- lib/diagnostico/recepcion_asedor_de_servicio/crear_cotizacion_funcionando/crear_cotizacion_funcionando_widget.dart
+++ lib/diagnostico/recepcion_asedor_de_servicio/crear_cotizacion_funcionando/crear_cotizacion_funcionando_widget.dart
@@ -681,11 +681,11 @@ class _CrearCotizacionFuncionandoWidgetState
                                                                     .fontStyle,
                                                               ),
                                                       ),
+                                                    ),
                                                     ],
                                                   ),
                                                 ),
                                               ),
-                                            ),
                                           Padding(
                                             padding: EdgeInsetsDirectional.fromSTEB(
                                                 0.0, 10.0, 0.0, 0.0),
