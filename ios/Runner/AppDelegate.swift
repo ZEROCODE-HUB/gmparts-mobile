4c2cdca2b8e26b9c43dff50eac6aaf00e9249859
unknown <andres@zerocode.la>
2026-08-08 14:51:43 -0500

    fix: registrar APNs remotos explicitamente en AppDelegate (swizzling de firebase_messaging)

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,8 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    UNUserNotificationCenter.current().delegate = UIApplication.shared.delegate as? UNUserNotificationCenterDelegate
+    UIApplication.shared.registerForRemoteNotifications()
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
